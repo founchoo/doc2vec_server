718b0e872a7d85d3da15a9e877200c583250ca58
Founchoo <founchoo@outlook.com>
2023-10-21 11:43:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,21 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import torch
 from transformers import BertTokenizer, BertModel
+import urllib.parse
 
 hostName = "localhost"
 serverPort = 8080
 model = None
 
 class Doc2Vec:
-    def __init__(self) -> None:
+    def __init__(self):
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         self.model = BertModel.from_pretrained('bert-base-uncased')
 
     def getVector(self, text: str) -> str:
         input_ids = torch.tensor(self.tokenizer.encode(text, add_special_tokens=True)).unsqueeze(0)
-        outputs = model(input_ids)
-        return str(outputs.pooler_output.detach().numpy()[0])
+        outputs = self.model(input_ids)
+        return str(outputs.pooler_output.detach().numpy().tolist()[0])
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -23,7 +24,9 @@ class MyServer(BaseHTTPRequestHandler):
         self.end_headers()
         if (self.path.startswith('/text=')):
             text = self.path[6:]
-            self.wfile.write(bytes(model.getVector(text), "utf-8"))
+            text = urllib.parse.unquote(text)
+            vector = model.getVector(text)
+            self.wfile.write(bytes(vector, "utf-8"))
 
 if __name__ == "__main__":
     model = Doc2Vec()
